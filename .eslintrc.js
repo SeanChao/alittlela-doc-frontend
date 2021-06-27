a653ff17dd3593ff71be0baaffd0a738e5a36066
SeanChao <seanchao0804@gmail.com>
2021-06-27 14:29:10 +0800

    init env

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  plugins: ["prettier", "simple-import-sort"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  globals: {
+    alert: true,
+    localStorage: true,
+    fetch: true,
+    test: true,
+    expect: true,
+    document: true,
+    window: true,
+    navigator: true,
+    cy: true,
+    it: true,
+    describe: true,
+    context: true,
+    beforeEach: true,
+    on: true,
+    config: true,
+  },
+};
